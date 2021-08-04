d7a307e97aabc72dbe709db5186c735404de1bb3
Lucas Bracco <braccolucas1393@gmail.com>
2021-08-04 18:47:33 -0300

    ADD: Start API

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`API connected successfully on port ${PORT}`);
+});
